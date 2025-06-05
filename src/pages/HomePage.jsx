4c88d42eff7f13931a9f008b8bcd0f585639820b
Rajesh Reddy Bejadi <64033035+BejadiRajeshReddy@users.noreply.github.com>
2025-06-05 10:48:22 +0530

    Updated HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,7 +20,8 @@ import {
   Leaf,
   Gavel,
 } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/Button";
 import companies from "../data/companies.json";
 // import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel";
 import { InfiniteSlider } from "@/components/ui/carousel";
